32c91001d95a5ab85b18f27d6bada4347adf1e53
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-05 19:46:16 +0600

    HashSetCreator is completed

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (Integer integer : sourceList) {
+            int buffer = 0;
+            if (integer % 2 == 0) {
+                buffer = integer;
+                result.add(buffer);
+                while (buffer % 2 == 0) {
+                    buffer /= 2;
+                    result.add(buffer);
+                }
+            } else {
+                result.add(integer);
+                result.add(integer * 2);
+            }
+        }
+        return result;
     }
 }
